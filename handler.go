bc62ffd88399a364110ec4fa3831cfba9f956cd3
Haroon Sheikh <haroon.sheikh@sky.uk>
2021-01-04 23:34:24 +0000

    Simplifies console output

--- handler.go
+++ handler.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/sitture/gauge-inprogress/gauge_messages"
 	"github.com/sitture/gauge-inprogress/helper"
 	"github.com/sitture/gauge-inprogress/logger"
 	"google.golang.org/grpc"
 	"os"
+	"strings"
 )
 
 type handler struct {
@@ -15,16 +15,33 @@ type handler struct {
 }
 
 func (h *handler) GenerateDocs(c context.Context, m *gauge_messages.SpecDetails) (*gauge_messages.Empty, error) {
-	specDirs := os.Getenv("GAUGE_SPEC_DIRS")
+	logger.Debugf("\n\nIn progress tags are set to %s.", helper.GetInProgressTags())
+	specDirs := strings.Split(strings.TrimSpace(os.Getenv("GAUGE_SPEC_DIRS")), "||")
 	logger.Debugf("Analysing specs under %s", specDirs)
-	allSpecs := helper.GetSpecs(m, helper.GetFiles(specDirs))
+	allSpecs := helper.GetSpecs(m, helper.GetSpecFiles(specDirs))
 	allScenarios := helper.GetScenarios(allSpecs)
 	inProgressSpecs, inProgressScenarios := helper.GetInProgressSpecsScenarios(allSpecs)
-
-	fmt.Printf("\nIn progress summary: %s", helper.GetProjectDirName())
-	fmt.Printf("\nSpecifications:\t%d of %d\t%d%%", len(inProgressSpecs), len(allSpecs), 10)
-	fmt.Printf("\nScenarios:\t%d of %d\t%d%%", len(inProgressScenarios), len(allScenarios), 14)
-
+	logger.Infof(
+		"In progress Summary: %s\n",
+		helper.GetProjectDirName(),
+	)
+	if helper.PercentOf(len(inProgressSpecs), len(allSpecs)) == 0 && helper.PercentOf(len(inProgressScenarios), len(allScenarios)) == 0 {
+		logger.Infof("No in progress scenarios found.")
+	} else {
+		logger.Infof(
+			"Specifications:\t%d of %d\t%0.0f%%",
+			len(inProgressSpecs),
+			len(allSpecs),
+			helper.PercentOf(len(inProgressSpecs), len(allSpecs)),
+		)
+		logger.Infof(
+			"Scenarios:\t%d of %d\t%0.0f%%",
+			len(inProgressScenarios),
+			len(allScenarios),
+			helper.PercentOf(len(inProgressScenarios), len(allScenarios)),
+		)
+	}
+	logger.Infof("")
 	return &gauge_messages.Empty{}, nil
 }
 
--- helper/helper.go
+++ helper/helper.go
@@ -1,91 +1,53 @@
 package helper
 
 import (
-	"fmt"
 	"github.com/getgauge/common"
 	"github.com/sitture/gauge-inprogress/gauge_messages"
 	"github.com/sitture/gauge-inprogress/logger"
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
+)
+
+const (
+	EnvFileExtensions = "gauge_spec_file_extensions"
+	EnvInProgressTags = "IN_PROGRESS_TAGS"
 )
 
 func GetProjectRoot() string {
 	return os.Getenv(common.GaugeProjectRootEnv)
 }
 
-func getAbsPath(path string) string {
-	f, err := filepath.Abs(path)
-	if err != nil {
-		logger.Fatalf("Cannot get absolute path")
-	}
-	return f
-}
-
-func GetProjectSpecsDir() string {
-	return getAbsPath(os.Getenv("gauge_specs_dir"))
-}
-
-func init() {
-	AcceptedExtensions[".spec"] = true
-	AcceptedExtensions[".md"] = true
-}
-
-var AcceptedExtensions = make(map[string]bool)
 var GetProjectDirName = func() string {
 	return path.Base(GetProjectRoot())
 }
 
-func isValidSpecExtension(path string) bool {
-	return AcceptedExtensions[filepath.Ext(path)]
-}
-
-func findFilesInDir(dirPath string, isValidFile func(path string) bool) []string {
-	var files []string
-	filepath.Walk(dirPath, func(path string, f os.FileInfo, err error) error {
-		if err == nil && !f.IsDir() && isValidFile(path) {
-			files = append(files, path)
+var GetInProgressTags = func() []string {
+	e := os.Getenv(EnvInProgressTags)
+	if e == "" {
+		e = "wip, in-progress" //this was earlier hardcoded, this is a failsafe if env isn't set
+	}
+	tags := strings.Split(strings.TrimSpace(e), ",")
+	var inProgressTags []string
+	for _, ext := range tags {
+		e := strings.TrimSpace(ext)
+		if e != "" {
+			inProgressTags = append(inProgressTags, e)
 		}
-		return err
-	})
+	}
+	return inProgressTags
+}
+
+// findFilesIn Finds all the files in the directory of a given extension
+func findFilesIn(dirRoot string, isValidFile func(path string) bool, shouldSkip func(path string, f os.FileInfo) bool) []string {
+	absRoot, _ := filepath.Abs(dirRoot)
+	files := common.FindFilesInDir(absRoot, isValidFile, shouldSkip)
 	return files
-}
-
-func findFilesIn(dirRoot string, isValidFile func(path string) bool) []string {
-	absRoot := getAbsPath(dirRoot)
-	files := findFilesInDir(absRoot, isValidFile)
-	return files
-}
-
-func dirExists(dirPath string) bool {
-	stat, err := os.Stat(dirPath)
-	if err == nil && stat.IsDir() {
-		return true
-	}
-	return false
-}
-
-func fileExists(path string) bool {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true
-	}
-	return !os.IsNotExist(err)
-}
-
-func GetFiles(path string) []string {
-	var specFiles []string
-	if dirExists(path) {
-		specFiles = append(specFiles, findFilesIn(path, isValidSpecExtension)...)
-	} else if fileExists(path) && isValidSpecExtension(path) {
-		specFiles = append(specFiles, getAbsPath(path))
-	}
-	return specFiles
 }
 
 func getProtoSpecs(m *gauge_messages.SpecDetails) []*gauge_messages.ProtoSpec {
 	specs := make([]*gauge_messages.ProtoSpec, 0)
-	fmt.Println(len(m.GetDetails()))
 	for _, detail := range m.GetDetails() {
 		if detail.GetSpec() != nil {
 			specs = append(specs, detail.GetSpec())
@@ -111,6 +73,61 @@ func GetSpecs(m *gauge_messages.SpecDetails, files []string) []*gauge_messages.P
 	return sortedSpecs
 }
 
+var GaugeSpecFileExtensions = func() []string {
+	e := os.Getenv(EnvFileExtensions)
+	if e == "" {
+		e = ".spec, .md" //this was earlier hardcoded, this is a failsafe if env isn't set
+	}
+	exts := strings.Split(strings.TrimSpace(e), ",")
+	var allowedExts = []string{}
+	for _, ext := range exts {
+		e := strings.TrimSpace(ext)
+		if e != "" {
+			allowedExts = append(allowedExts, e)
+		}
+	}
+	return allowedExts
+}
+
+// IsValidSpecExtension Checks if the path has a spec file extension
+func IsValidSpecExtension(path string) bool {
+	for _, ext := range GaugeSpecFileExtensions() {
+		if ext == strings.ToLower(filepath.Ext(path)) {
+			return true
+		}
+	}
+	return false
+}
+
+// FindSpecFilesIn Finds spec files in the given directory
+var FindSpecFilesIn = func(dir string) []string {
+	return findFilesIn(dir, IsValidSpecExtension, func(path string, f os.FileInfo) bool {
+		return false
+	})
+}
+
+// GetSpecFiles returns the list of spec files present at the given path.
+// If the path itself represents a spec file, it returns the same.
+var GetSpecFiles = func(paths []string) []string {
+	var specFiles []string
+	for _, path := range paths {
+		if !common.FileExists(path) {
+			logger.Fatalf("Specs directory %s does not exists.", path)
+		}
+		if common.DirExists(path) {
+			specFilesInPath := FindSpecFilesIn(path)
+			if len(specFilesInPath) < 1 {
+				logger.Fatalf("No specifications found in %s.", path)
+			}
+			specFiles = append(specFiles, specFilesInPath...)
+		} else if IsValidSpecExtension(path) {
+			f, _ := filepath.Abs(path)
+			specFiles = append(specFiles, f)
+		}
+	}
+	return specFiles
+}
+
 func GetScenarios(specs []*gauge_messages.ProtoSpec) []*gauge_messages.ProtoScenario {
 	scenarios := make([]*gauge_messages.ProtoScenario, 0)
 	for _, spec := range specs {
@@ -130,7 +147,7 @@ func GetInProgressSpecsScenarios(specs []*gauge_messages.ProtoSpec) (
 	inProgressSpecs = make(map[string]*gauge_messages.ProtoSpec, 0)
 	inProgressScenarios = make(map[string]*gauge_messages.ProtoScenario, 0)
 	for _, spec := range specs {
-		if find(spec.GetTags(), "wip") {
+		if containsInProgressTags(spec.GetTags()) {
 			if _, exists := inProgressSpecs[spec.GetFileName()]; !exists {
 				inProgressSpecs[spec.GetFileName()] = spec
 			}
@@ -143,7 +160,7 @@ func GetInProgressSpecsScenarios(specs []*gauge_messages.ProtoSpec) (
 		} else {
 			for _, itemType := range spec.GetItems() {
 				scenario := itemType.GetScenario()
-				if scenario != nil && find(scenario.GetTags(), "wip") {
+				if scenario != nil && containsInProgressTags(scenario.GetTags()) {
 					inProgressScenarios[scenario.GetScenarioHeading()] = scenario
 					if _, exists := inProgressSpecs[spec.GetFileName()]; !exists {
 						inProgressSpecs[spec.GetFileName()] = spec
@@ -156,11 +173,17 @@ func GetInProgressSpecsScenarios(specs []*gauge_messages.ProtoSpec) (
 	return
 }
 
-func find(slice []string, val string) bool {
-	for _, item := range slice {
-		if item == val {
-			return true
+func containsInProgressTags(tags []string) bool {
+	for _, inProgressTag := range GetInProgressTags() {
+		for _, tag := range tags {
+			if strings.TrimSpace(tag) == inProgressTag {
+				return true
+			}
 		}
 	}
 	return false
+}
+
+func PercentOf(part int, total int) float64 {
+	return (float64(part) * float64(100)) / float64(total)
 }
